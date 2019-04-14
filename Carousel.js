01483813fddba6591a6acdeeb8d9dc718ad676fc
Jihyun <ji_hyun5@hotmail.com>
2019-04-14 18:39:45 +0100

    Initial carousel design
    Added images
    Only arrows with mouse click

---
+++ Carousel.js
@@ -0,0 +1,39 @@
+const sliderImages = document.querySelectorAll(".slide");
+const arrowLeft = document.querySelector("#arrow-left");
+const arrowRight = document.querySelector("#arrow-right");
+let currentImg = 0;
+
+function clearImages() {
+  sliderImages.forEach(img => { img.style.display = "none" })
+}
+
+function startSlide() {
+  clearImages();
+  sliderImages[0].style.display = "block";
+}
+
+function slideLeft() {
+  clearImages();
+  if (currentImg === 0) {
+    currentImg = sliderImages.length;
+  }
+  sliderImages[currentImg - 1].style.display = "block";
+  currentImg--;
+}
+
+function slideRight() {
+  clearImages();
+  if (currentImg === sliderImages.length - 1) {
+    currentImg = -1;
+  }
+  sliderImages[currentImg + 1].style.display = "block";
+  currentImg++;
+}
+
+// Left arrow click
+arrowLeft.addEventListener("click", slideLeft);
+
+// Right arrow click
+arrowRight.addEventListener("click", slideRight);
+
+startSlide();
